f9ab50e07447b3e32c4f5c1efd53843c21636883
Patryk Panek <patryyk221@gmail.com>
2022-11-18 18:12:01 +0100

    added reuseable component Button and added component Business

--- modern-website/src/components/Business.jsx
+++ modern-website/src/components/Business.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { features } from '../constants'
+import styles, {layout} from '../styles'
+import Button from './Button'
 
-const Buisness = () => {
-  return (
-    <div>Buisness</div>
+const FeatureCard = ({icon, title, content, index}) => (
+  <div>
+    <div>
+      <img src={icon} alt="icon" />
+    </div>
+  </div>
+)
+
+const Buisness = () =>  (
+    <section id='features' className={`${layout.section}`}>
+        <div className={`${layout.sectionInfo}`}>
+          <h2 className={`${styles.heading2}`}>
+          You do the business,  
+          <br className='sm:block hidden'/>
+            we'll handle the money.
+          </h2>
+          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+          <Button styles='mt-10'/>
+        </div>
+
+        <div className={`${layout.sectionImg} flex-col `}>
+          {features.map((feature, index) =>(
+            <FeatureCard key={feature.id} {...feature} index={index}/>
+          ))}
+        </div>
+    </section>
   )
-}
+
 
 export default Buisness
