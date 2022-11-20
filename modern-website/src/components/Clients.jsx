8925b02091a97dd1ad3db59b3e5598198066c1b4
Patryk Panek <patryyk221@gmail.com>
2022-11-20 14:59:04 +0100

    added rest components to the website

--- modern-website/src/components/Clients.jsx
+++ modern-website/src/components/Clients.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../styles'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+
+const Clients = () =>  (
+    <section className={`${styles.flexCenter} m-4`}>
+        <div className={`${styles.flexCenter} flex-wrap w-full`}>
+          {clients.map((client)=>(
+            <div className={`flex-1 ${styles.flexCenter} sm:min-w-[200px] min-w-[120px] sm:mt-5 mt-4`} key={client.id}>
+              <img src={client.logo} alt="client" className='sm:w-[200px] w-[100px] object-contain' />
+            </div>
+          ))}
+        </div>
+    </section>
   )
-}
+
 
 export default Clients
